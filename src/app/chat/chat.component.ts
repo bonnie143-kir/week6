015f083dba3e60b74d6998612196d4d7b779d35b
Bongraoi Arebaio <bongraoi.arebaio@griffithuni.edu.au>
2022-09-15 15:59:33 +1000

    Added variables from form in the ts file

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./chat.component.css']
 })
 export class ChatComponent implements OnInit {
+  messagecontent:string="";
+  messages: string="";
+  rooms=[];
+  roomslist:string="";
+  roomnotice:string="";
+  currentroom:string="";
+  isinRoom=false;
+  newroom:string="";
+  numusers:number=0;
 
   constructor() { }
 
